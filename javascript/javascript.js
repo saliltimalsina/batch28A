f56dd7a77c11f10731ebcaedf6851904977e6865
pradipkandel008 <pradeepkandel008@gmail.com>
2021-07-03 21:19:04 +0545

    javascript

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  console.log("Button clicked!");
+  window.alert("Button click alert!");
+}
+
+document.write("This is content from javascript");
+
+document.getElementById("para1").innerHTML = "Paragraph from javascript";
